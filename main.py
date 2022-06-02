062354aeb9e83dcaa01cec8e3c772982a1459a5e
k1b3r74rd <kibertard@gmail.com>
2022-06-02 14:45:32 +0200

    bot connects to voice channel and plays sounds. next step: involve radio website.

--- main.py
+++ main.py
@@ -2,9 +2,10 @@ from configparser import ConfigParser
 import logging
 import discord
 from discord.ext import commands
-import random, datetime
-# TODO: Bot connecting to voice channel. Playing sounds in it.
-# TODO: Playing sounds from web. (for example: youtube, vk, soundcloud?)
+
+# TODO: sounds from youtube || vk || soundcloud
+# TODO: start involving radio.garden
+
 
 class Config:
     """
@@ -21,12 +22,16 @@ class Config:
         return access_token
 
 
+# Logging information into logs.log file
+# TODO: cleaner log file
 logging.basicConfig(format=u'%(levelname)-9s [%(asctime)s]  %(message)s',
                     level=logging.INFO, filename='logs.log')
 
+# Getting personal information from config.ini
 getconfig = Config()
 TOKEN = getconfig.get_access_token()
 
+# Preparing API and settings for commands for bot
 client = commands.Bot(command_prefix='%')
 client.remove_command('help')
 
@@ -78,17 +83,43 @@ async def help(ctx):
         inline=False
     )
     embed.add_field(
+        name='%botping',
+        value="Checks bot ping \n ⠀",
+        inline=False
+    )
+    embed.add_field(
         # https://thiscatdoesnotexist.com/
         name='%cat',
-        value="This cat doesn't exist. Doesn't work propely as for me. \n ⠀",
+        value="This cat doesn't exist. Doesn't work propely as for me. \n",
         inline=False
     )
     embed.add_field(
         # https://thisartworkdoesnotexist.com/
         name='%art',
-        value="This art doesn't exist. Doesn't work propely as for me. \n ",
+        value="This art doesn't exist. Doesn't work propely as for me. \n ⠀",
         inline=False
     )
+    embed.add_field(
+        name='%join',
+        value="Voice command. Joins to voice channel. \n ",
+        inline=False
+    )
+    embed.add_field(
+        name='%perfect',
+        value="Voice command. Plays 'Perfect' quote from Quake 3 Arena. \n ",
+        inline=False
+    )
+    embed.add_field(
+        name='%perfect',
+        value="Voice command. Plays 'Fresh meat' quote from DOTA 2. \n ",
+        inline=False
+    )
+    embed.add_field(
+        name='%disconnect',
+        value="Voice command. Disconnects from voice channel. \n ⠀",
+        inline=False
+    )
+
     await ctx.send(embed=embed)
 
     logging_response(ctx, embed)
@@ -122,6 +153,100 @@ async def art(ctx):
     await ctx.send(content=content)
 
     logging_response(ctx, content)
+
+
+@client.command()
+async def botping(ctx):
+    """
+    %botping
+    """
+    command = r'%botping'
+    logging_message(ctx, command)
+
+    ping_raw = client.latency
+    ping = round(ping_raw * 1000)
+    content = f'My ping is {ping}ms'
+    await ctx.send(content)
+
+    logging_response(ctx, content)
+
+
+@client.command(pass_context = True)
+async def join(ctx):
+    """
+    %join - joins voice channel to summoner
+    """
+    command = r'%join'
+    logging_message(ctx, command)
+
+    if ctx.author.voice:
+        voice_channel = ctx.message.author.voice.channel
+        await voice_channel.connect()
+        await ctx.send(r'Joined.')
+    else:
+        await ctx.send(r'You are not in a voice channel to use this command.')
+
+    logging_response(ctx, r'Reply sent.') # TODO: fix message and response sending
+
+
+@client.command(pass_context = True)
+async def disconnect(ctx):
+    """
+    %disconnect - leaves voice channel
+    """
+    command = r'%disconnect'
+    logging_message(ctx, command)
+
+    if ctx.voice_client:
+        await ctx.guild.voice_client.disconnect()
+        await ctx.send(r'Disconnected.')
+    else:
+        await ctx.send(r'Not in a voice channel.')
+
+    logging_response(ctx, 'Reply sent') # TODO: fix message and response sending
+
+
+@client.command(pass_context = True)
+async def perfect(ctx):
+    """
+    %perfect - plays "perfect" quote from Quake 3 Arena. WAV file on PC.
+    """
+    command = r'%perfect'
+    logging_message(ctx, command)
+
+    guild = ctx.guild
+    voice_client: discord.VoiceClient = discord.utils.get(client.voice_clients, guild=guild)
+
+    source = r'perfect.wav'
+    audio_source = discord.FFmpegPCMAudio(source)
+
+    if not voice_client.is_playing():  # TODO: queue?
+        voice_client.play(audio_source, after=None)
+        await ctx.send('Perfect.')
+
+    logging_response(ctx, 'Sound involved: Perfect.')
+
+
+@client.command(pass_context = True)
+async def freshmeat(ctx):
+    """
+    %freshmeat - plays "fresh meat" quote from DOTA 2. MP3 file on website.
+    """
+    command = r'%freshmeat'
+    logging_message(ctx, command)
+
+    guild = ctx.guild
+    voice_client: discord.VoiceClient = discord.utils.get(client.voice_clients, guild=guild)
+
+    source = r"https://static.wikia.nocookie.net/dota2_ru_gamepedia/images/4/48/Pud_ability_devour_02_ru.mp3/revision" \
+             r"/latest?cb=20170413110428 "
+    audio_source = discord.FFmpegPCMAudio(source=source)
+
+    if not voice_client.is_playing():  # TODO: queue?
+        voice_client.play(audio_source, after=None)
+        await ctx.send('Fresh meat.')
+
+    logging_response(ctx, 'Sound involved: Fresh meat.')
 
 
 if __name__ == '__main__':
